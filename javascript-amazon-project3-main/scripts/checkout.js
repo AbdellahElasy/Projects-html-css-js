854647a0286a3c7728d5e855490423bb2939fdac
AbdellahElasy <abdellahelasry97@gmail.com>
2025-09-25 22:04:20 +0100

    localStorage , deletebutton

--- javascript-amazon-project3-main/scripts/checkout.js
+++ javascript-amazon-project3-main/scripts/checkout.js
@@ -1,38 +1,41 @@
-import {cart} from "../data/cart.js";
+import {cart,removeFormCart} from "../data/cart.js";
 import {products} from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+let cartSummaryHtml = '';
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
   let matchingProduct;
-  products.forEach((products) => {
-  if(products.id === productId){
-    matchingProduct =products;
+  products.forEach((product) => {
+  if(product.id === productId){
+    matchingProduct =product;
   }
   });
-`
-<div class="cart-item-container">
+  cartSummaryHtml +=`
+<div class="cart-item-container 
+js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
 
             <div class="cart-item-details-grid">
               <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+                src="${matchingProduct.image}">
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -45,7 +48,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -58,7 +61,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -71,7 +74,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -84,7 +87,18 @@ cart.forEach((cartItem) => {
               </div>
             </div>
           </div>
-`
+`;
 
 
+});
+document.querySelector('js-order-summary').innerHTML = cartSummaryHtml;
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+link.addEventListener('click' , () =>{
+  const productId = link.dataset.productId;
+  removeFormCart(productId);
+ const container= document.querySelector(`.js-cart-item-container-${matchingProduct.id}`)
+ container.remove();
+})
 })
